f4cba146425c24a4a61eb255bf65b5a484940e85
nch303 <haoncse184899@fpt.edu.vn>
2025-09-17 22:52:19 +0700

    update

--- src/gateway/ApiGateway/Program.cs
+++ src/gateway/ApiGateway/Program.cs
@@ -1,39 +1,4 @@
-﻿//using Microsoft.OpenApi.Models;
-//using Ocelot.DependencyInjection;
-//using Ocelot.Middleware;
-//using MMLib.SwaggerForOcelot.DependencyInjection;
-//using MMLib.SwaggerForOcelot.Middleware;
-
-//var builder = WebApplication.CreateBuilder(args);
-
-//// Add Ocelot
-//builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
-//builder.Services.AddOcelot(builder.Configuration);
-
-//// Add Swagger for Ocelot
-//builder.Services.AddSwaggerForOcelot(builder.Configuration)
-//    .AddSwaggerGen(opt =>
-//    {
-//        opt.SwaggerDoc("v1", new OpenApiInfo
-//        {
-//            Title = "Api Gateway",
-//            Version = "v1"
-//        });
-//    });
-
-
-//var app = builder.Build();
-
-//// Enable SwaggerForOcelot
-//app.UseSwaggerForOcelotUI();
-
-//// Enable Ocelot
-//await app.UseOcelot();
-
-//app.Run();
-
-
-using Microsoft.OpenApi.Models;
+﻿using Microsoft.OpenApi.Models;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using MMLib.SwaggerForOcelot.DependencyInjection;
@@ -41,33 +6,29 @@ using MMLib.SwaggerForOcelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// load cấu hình ocelot.json
-builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
-
 // Add Ocelot
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 builder.Services.AddOcelot(builder.Configuration);
 
-// Add Swagger + SwaggerForOcelot
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo
+// Add Swagger for Ocelot
+builder.Services.AddSwaggerForOcelot(builder.Configuration)
+    .AddSwaggerGen(opt =>
     {
-        Title = "Api Gateway",
-        Version = "v1"
+        opt.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Title = "Api Gateway",
+            Version = "v1"
+        });
     });
-});
 
-builder.Services.AddSwaggerForOcelot(builder.Configuration);
 
 var app = builder.Build();
 
-// SwaggerForOcelot UI
-app.UseSwaggerForOcelotUI(opt =>
-{
-    opt.PathToSwaggerGenerator = "/swagger/docs"; // mặc định
-});
+// Enable SwaggerForOcelot
+app.UseSwaggerForOcelotUI();
 
-// Ocelot
+// Enable Ocelot
 await app.UseOcelot();
 
 app.Run();
+
