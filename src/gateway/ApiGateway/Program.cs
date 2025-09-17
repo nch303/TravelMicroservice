556d900f08eb851c3b172079ea0e884bdda58424
nch303 <haoncse184899@fpt.edu.vn>
2025-09-17 23:20:14 +0700

    update

--- src/gateway/ApiGateway/Program.cs
+++ src/gateway/ApiGateway/Program.cs
@@ -1,29 +1,20 @@
-﻿using Microsoft.OpenApi.Models;
-using Ocelot.DependencyInjection;
+﻿using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using MMLib.SwaggerForOcelot.DependencyInjection;
 using MMLib.SwaggerForOcelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Load cấu hình Ocelot
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
-// Đăng ký Ocelot
+// Chỉ cần cái này, KHÔNG thêm AddSwaggerGen nữa
 builder.Services.AddOcelot(builder.Configuration);
-
-// Đăng ký Swagger + SwaggerForOcelot
 builder.Services.AddSwaggerForOcelot(builder.Configuration);
 
 var app = builder.Build();
 
-// Swagger UI cho Ocelot
-app.UseSwaggerForOcelotUI(opt =>
-{
-    opt.PathToSwaggerGenerator = "/swagger/docs"; // bắt buộc với SwaggerForOcelot
-});
+// Enable swagger ui cho Ocelot
+app.UseSwaggerForOcelotUI();
 
-// Middleware Ocelot
 await app.UseOcelot();
-
 app.Run();
