aa70269dd626283c4277c58a7163c43c87f3c17b
nch303 <haoncse184899@fpt.edu.vn>
2025-09-17 21:05:29 +0700

    setup_ocelot

--- src/gateway/ApiGateway/Program.cs
+++ src/gateway/ApiGateway/Program.cs
@@ -1,37 +1,33 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.OpenApi.Models;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
-using Microsoft.OpenApi.Models;
+using MMLib.SwaggerForOcelot.DependencyInjection;
+using MMLib.SwaggerForOcelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Thêm Ocelot
-builder.Configuration.AddJsonFile("ocelot.json");
+// Add Ocelot
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 builder.Services.AddOcelot(builder.Configuration);
 
-// Thêm Swagger với cấu hình đầy đủ
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo
+// Add Swagger for Ocelot
+builder.Services.AddSwaggerForOcelot(builder.Configuration)
+    .AddSwaggerGen(opt =>
     {
-        Title = "ApiGateway",
-        Version = "v1",
-        Description = "Gateway for microservices"
+        opt.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Title = "Api Gateway",
+            Version = "v1"
+        });
     });
-});
+
 
 var app = builder.Build();
 
-// Sử dụng Ocelot
-app.UseOcelot().Wait();
+// Enable SwaggerForOcelot
+app.UseSwaggerForOcelotUI();
 
-// Sử dụng Swagger
-app.UseSwagger();
-app.UseSwaggerUI(c =>
-{
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "ApiGateway V1");
-    c.SwaggerEndpoint("http://localhost:7019/swagger/v1/swagger.json", "UserService API V1"); 
-    //Add Service Here
-});
+// Enable Ocelot
+await app.UseOcelot();
 
 app.Run();
